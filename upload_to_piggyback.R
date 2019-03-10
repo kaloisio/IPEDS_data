69d073892018999b99f9b984e00c2cc4b68da7c2
kaloisio <kaloisio@gmail.com>
2019-03-10 15:19:33 -0400

    tried piggyback, web scrapping and compiling

--- upload_to_piggyback.R
+++ upload_to_piggyback.R
@@ -1,3 +1,77 @@
 # Script to initialize and upload data to piggyback
 
 install.packages("piggyback")
+
+Sys.setenv(GITHUB_TOKEN="6807c1c8f073215bc9416966295e67b951c3f8de")
+
+pb_new_release("kaloisio/IPEDS_data", "v0.0.1")
+
+
+readr::write_tsv(mtcars, "mtcars.tsv.gz")
+
+pb_upload("mtcars.tsv.gz", 
+          repo = "kaloisio/IPEDS_data", 
+          tag = "v0.0.1")
+
+
+pb_delete(file = "mtcars.tsv.gz", 
+          repo = "kaloisio/IPEDS_data", 
+          tag = "v0.0.1")
+
+
+pb_download("mtcars.tsv.gz", 
+            repo = "kaloisio/IPEDS_data",
+            tag = "v0.0.1",
+            dest = tempdir())
+
+
+test <- read.table(gzfile(paste0(tempdir(), "/mtcars.tsv.gz")))
+
+
+dir('C:/Users/kaloisio/Desktop/c_a/Data/')
+
+files2zip <- dir('C:/Users/kaloisio/Desktop/c_a/Data/', full.names = TRUE)
+zip(zipfile = 'testZip', files = files2zip)
+
+
+file.info(c('testZip.zip', 'testDir/test.csv'))['size']
+
+
+install.packages("rvest")
+library('rvest')
+
+#Specifying the url for desired website to be scraped
+url <- 'https://nces.ed.gov/ipeds/datacenter/DataFiles.aspx'
+
+#Reading the HTML code from the website
+webpage <- read_html(url)
+
+require(devtools)
+install_github('jbryer/ipeds')
+
+
+temp <- tempfile()
+download.file("https://nces.ed.gov/ipeds/datacenter/data/ADM2017.zip",temp)
+con <- unz(temp, "adm2017.csv")
+data <- matrix(scan(con),ncol=4,byrow=TRUE)
+unlink(temp)
+
+year = 2016
+survey = "Adm"
+
+zipfile = paste0("https://nces.ed.gov/ipeds/datacenter/data/", toupper(survey), year, ".zip")
+csvfile = paste0(tolower(survey), year, ".csv")
+
+
+temp <- tempfile()
+download.file(zipfile,temp)
+data <- read.csv(unz(temp, csvfile))
+unlink(temp)
+
+names(data)
+
+
+#https://nces.ed.gov/ipeds/datacenter/data/ADM2017_Dict.zip
+
+
+#cant compile on my laptop
